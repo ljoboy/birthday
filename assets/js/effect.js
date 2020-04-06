30039e77bd24328b52ddf6500f4eacf999765dba
Dark Angel <jonathanyombo@gmail.com>
2020-04-06 22:19:20 +0200

    [UPDATE] more perfect

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -146,12 +146,12 @@ $('document').ready(function(){
 		$('#b6').attr('id','b66');
 		$('#b7').attr('id','b77');
 		$('#b11').animate({top:240, left: vw-220},500);
-		$('#b22').animate({top:240, left: vw-160},500);
-		$('#b33').animate({top:240, left: vw-110},500);
-		$('#b44').animate({top:240, left: vw},500);
-		$('#b55').animate({top:240, left: vw+40},500);
-		$('#b66').animate({top:240, left: vw+80},500);
-		$('#b77').animate({top:240, left: vw+120},500);
+		$('#b22').animate({top:240, left: vw-180},500);
+		$('#b33').animate({top:240, left: vw-150},500);
+		$('#b44').animate({top:240, left: vw-50},500);
+		$('#b55').animate({top:240, left: vw},500);
+		$('#b66').animate({top:240, left: vw+50},500);
+		$('#b77').animate({top:240, left: vw+100},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
